2c91ca7e484a191b50eded3f8a9f3e4a22495a9d
Jason Ha <jph7861@g.rit.edu>
2026-01-29 10:16:03 -0500

    Creating Database for storage manager

---
+++ StorageManager/Database.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+
+public class Database{
+
+    //Each Database will have a number of pages and the name of the database
+    public static String database_name;
+    public static int database_buffer_size;
+
+    public Database(String name, int size_bytes) throws IOException {
+        database_name = name;
+        database_buffer_size = size_bytes;
+
+        //Create a RandomAccessFile
+        RandomAccessFile database_access = new RandomAccessFile(database_name,"rw");
+        byte[] database = new byte[database_buffer_size];
+        database_access.write(database);
+        database_access.close();
+
+    }
+
+    public static String[] exist_database(String name){
+        database_name = name;
+        File database_file = new File(database_name);
+        if(database_file.exists()){
+            return new String[]{"True", String.valueOf(database_file.length())};
+        }
+        else{
+            return new String[]{"False"};
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        Database test = new Database("test", 1000);
+        System.out.println(Arrays.toString(exist_database("test")));
+    }
+}
