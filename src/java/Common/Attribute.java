cd8b91ca007b83d7caadfce8a85f42c87e7a55f4
Eliana <egd1486@rit.edu>
2026-02-05 08:53:57 -0500

    enum added to attribute

--- src/java/Common/Attribute.java
+++ src/java/Common/Attribute.java
@@ -2,17 +2,17 @@ package Common;
 
 public class Attribute { //for one column
     private String name;
-    private String type; //incorportate enum type
-    private int typeLength; // ! how to store 5 in varchar(5) ?
-    private boolean privateKey; //is privateKey
+    private Type type; 
+    private int typeLength; 
+    private boolean primaryKey; //is privateKey
     private boolean notNull; //is not null
     private boolean unique; //is unique
 
-    public Attribute(String name, String type, int typeLength, boolean privateKey, boolean notNull, boolean unique) {
+    public Attribute(String name, Type type, int typeLength, boolean primaryKey, boolean notNull, boolean unique) {
         this.name = name;
         this.type = type;
         this.typeLength = typeLength;
-        this.privateKey = privateKey;
+        this.primaryKey = primaryKey;
         this.notNull = notNull;
         this.unique = unique;
     }
