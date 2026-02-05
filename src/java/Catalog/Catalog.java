c8444365d7671af5cd13e7efa1e33fcb76a4fee0
Joe C <jrc8407@rit.edu>
2026-02-05 09:00:07 -0500

    Added Catalog functions

--- src/java/Catalog/Catalog.java
+++ src/java/Catalog/Catalog.java
@@ -1,5 +1,19 @@
 package Catalog;
 
+import java.util.ArrayList;
+
 public class Catalog {
-    
+    private static ArrayList<Schema> Schemas = new ArrayList<Schema>();
+
+    public Boolean AddSchema(String Name) {
+        return true;
+    }
+
+    public Schema GetSchema(String Name) {
+        return null;
+    }
+
+    public Boolean RemoveSchema(String Name) {
+        return true;
+    }
 }
