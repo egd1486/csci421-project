5ff61d322050a3bcdd9fe181e4bc6cbf7db2900d
Eliana <egd1486@rit.edu>
2026-02-05 08:47:55 -0500

    touched table and attribute

--- src/java/Common/Attribute.java
+++ src/java/Common/Attribute.java
@@ -1,5 +1,19 @@
 package Common;
 
-public class Attribute {
-    
+public class Attribute { //for one column
+    private String name;
+    private String type; //incorportate enum type
+    private int typeLength; // ! how to store 5 in varchar(5) ?
+    private boolean privateKey; //is privateKey
+    private boolean notNull; //is not null
+    private boolean unique; //is unique
+
+    public Attribute(String name, String type, int typeLength, boolean privateKey, boolean notNull, boolean unique) {
+        this.name = name;
+        this.type = type;
+        this.typeLength = typeLength;
+        this.privateKey = privateKey;
+        this.notNull = notNull;
+        this.unique = unique;
+    }
 }
--- src/java/Common/Table.java
+++ src/java/Common/Table.java
@@ -1,5 +1,16 @@
 package Common;
 
+import java.util.ArrayList;
+
 public class Table {
-    
+    private String name;
+    private ArrayList<Attribute> attributes; 
+
 }
+
+//  Table "students" (TableSchema)
+// │   ├── Attributes: id, name, gpa, major
+// │   ├── Record 1: (123, "Alice", 3.5, "CS")
+// │   ├── Record 2: (124, "Bob", 3.8, "Math")
+// │   └── ... (stored across Pages 5, 6, 7...)
+
