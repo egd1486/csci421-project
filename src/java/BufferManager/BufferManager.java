6f5d3651228678a1b010fffc373c20fbe5cfe0b3
WinterSeasonn <jasonha567@gmail.com>
2026-02-15 14:11:06 -0500

    buffer

---
+++ src/java/BufferManager/BufferManager.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+public class BufferManager {
+
+    /*
+    BufferManager
+    Get Frame size to know what pages are being used and be accessed.
+    Track Dirty Pages
+    Implement LRU
+    Decide when a page must be written back
+
+
+
+     */
+
+    public static int BufferSize;
+
+    Frame[] buffer = null;
+    List<Integer, >
+
+    public BufferManager(int set_buffer_size){
+        buffer = new Frame[set_buffer_size];
+    }
+
+
+}
