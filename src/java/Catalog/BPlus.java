d25723a84c05b20a7b21752a23165eb4bff3050c
Joe C <jrc8407@rit.edu>
2026-04-21 11:34:40 -0400

    pushing changes from laptop

--- src/java/Catalog/BPlus.java
+++ src/java/Catalog/BPlus.java
@@ -5,6 +5,7 @@ import Common.Attribute;
 import java.util.ArrayList;
 import BufferManager.BufferManager;
 import StorageManager.StorageManager;
+import java.lang.reflect.Array;
 
 public class BPlus {
     Schema Schema;
@@ -19,19 +20,72 @@ public class BPlus {
         this.Root = (Root != null) ? Root:StorageManager.CreatePage();
     }
 
-    public void Insert(Object Key) {
-        // TODO traverse the tree to insert this search key.
+    // Returns the Leaf node that should contain a given key.
+    private Page FindLeaf(Comparable<Object> Key) throws Exception {
+        // Traverse the tree to the node that should contain the search key, and return it.
+        Page Current = BufferManager.getBNode(Root, Attribute);
+
+        while (!Current.leafnode) {
+            ArrayList<ArrayList<Object>> Rows = Current.get_data();
+
+            boolean NotNext = false; // Mark if sub-page found, otherwise next page id used.
+            for (ArrayList<Object> Row : Rows) {
+                Comparable<Object> CKey = (Comparable<Object>) Row.get(1);
+
+                // Update the notnext flag, and check if we found the right child.
+                if (NotNext = Key.compareTo(CKey) >= 0) {
+                    Current = BufferManager.getBNode((Integer) Row.get(0), Attribute);
+                    break;
+                }
+            }
+
+            if (!NotNext) Current = BufferManager.getPage(Current.get_next_pageid(), this.Schema);
+        }
+
+        return Current;
     }
 
-    public Page Find(Object Key) {
-        // TODO traverse the tree to find the page that contains this search key.
+    public void Insert(Object Key) throws Exception {
+        
+        Page Leaf = FindLeaf((Comparable<Object>) Key);
+
+        // If key already in this leaf, throw.
+        if (this.Find((Comparable<Object>) Key, Leaf) != null) 
+        throw new Exception ("Key already exists in the tree.");
+
+        //Otherwise, handle insertion logic:
+
+    }
+
+    // Returns the page id containing the key. Null if not found.
+    public Integer Find(Comparable<Object> Key, Page Leaf) throws Exception {
+        // If leaf is not given, use FindLeaf, otherwise use what's provided.
+        Leaf = (Leaf == null) ? FindLeaf(Key) : Leaf;
+
+        // Get the rows,
+        ArrayList<ArrayList<Object>> Rows = Leaf.get_data();
+
+        // Return if key is found in this leaf node,
+        // For each row,
+        for (int i=0; i<Rows.size(); i++) 
+
+        // If the key is found,
+        if (Rows.get(i).get(1).equals(Key))
+
+        // Return next page pointer, or nextpage pointer if at the end of the rows.
+        return (i+1 != Rows.size()) ? (Integer) Rows.get(i+1).get(0) : Leaf.get_next_pageid();
+
+        // Otherwise we know this key is not in the tree, return null.
         return null;
     }
 
     // Direction: 0 for equal, -1 for less than, 1 for greater than.
     public ArrayList<Integer> Filter(Object Key, int Direction) {
-        // TODO traverse the tree to find pages that satisfy the filter condition.
-        return null;
+        ArrayList<Integer> Result = new ArrayList<Integer>();
+
+
+
+        return Result;
     }
 
     public void Clear() {
