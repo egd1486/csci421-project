a48bed3ff46a8d27870c9cfb10f965fff8092298
Joe <jrc8407@g.rit.edu>
2026-04-23 16:53:23 -0400

    patch

--- src/java/Catalog/Schema.java
+++ src/java/Catalog/Schema.java
@@ -148,7 +148,7 @@ public class Schema {
         
         Attribute A = new Attribute(Name, T, Size, isPrimary, isNullable, isUnique, Default);
         // Attribute A = new Attribute(Name, T, Size, Primary, Nullable, Unique, Default);
-        if(!Primary && Unique && Parser.Indexing) {
+        if(isPrimary && isUnique && Parser.Indexing) {
             A.BPlusTree = new BPlus(this, A, null);
             A.bTree = A.BPlusTree.Root;  
         }
